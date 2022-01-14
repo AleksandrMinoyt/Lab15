16ba1a957594d3c040a7e8011760263bd9ac3aac
fox_itleksandr <fox_it@bk.ru>
2022-01-14 10:29:26 +0300

    Add project files.

---
+++ Lab15/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression ap = new ArithProgression(1,2);
+            
+            Console.WriteLine(ap.GetNext());
+            Console.WriteLine(ap.GetNext());
+            Console.WriteLine(ap.GetNext());
+
+            Console.WriteLine();
+
+            ap.Reset();
+            ap.SetStart(3);          
+            Console.WriteLine(ap.GetNext());
+            Console.WriteLine(ap.GetNext());
+            Console.WriteLine(ap.GetNext());
+
+            Console.WriteLine();
+
+            GeomProgression gp = new GeomProgression(1, 2);
+
+            Console.WriteLine(gp.GetNext());
+            Console.WriteLine(gp.GetNext());
+            Console.WriteLine(gp.GetNext());
+
+            gp.Reset();
+            gp.SetStart(3);
+            Console.WriteLine();
+            Console.WriteLine(gp.GetNext());
+            Console.WriteLine(gp.GetNext());
+            Console.WriteLine(gp.GetNext());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lab15/Progressions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class ArithProgression : ISeries
+    {
+        protected int _start;  //старт прогрессии
+        protected int _n=1;    //текущий член прогрессии
+        protected int _step;   //шаг(знаменатель) прогрессии
+
+        public int Step
+        {
+            get
+            {
+                return _step;
+            }
+
+            set
+            {
+                _step = value;
+            }
+        }
+
+        public virtual int GetN(int n)
+        {
+            return _start + (n -1) * _step;
+        }
+
+        public int GetNext()
+        {
+            _n++;
+            return GetN(_n);
+        }
+
+        public void Reset()
+        {
+            _n = 1;
+        }
+
+        public void SetStart(int x)
+        {
+            _start = x;
+        }
+        public ArithProgression(int start, int step)
+        {
+            _start = start;
+            _step = step;
+            
+        }
+    }
+
+    class GeomProgression : ArithProgression
+        
+    {
+        public GeomProgression(int start, int step)
+            :base (start, step)
+        {}
+
+        public override int GetN(int n)
+        {
+            return _start * (int)Math.Pow(_step, n - 1);
+        }
+
+    }
+
+}
